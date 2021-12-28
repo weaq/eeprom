c1ada161f2c08f379000ea19ae6516d16d326459
weaq <30413309+weaq@users.noreply.github.com>
2021-12-28 21:43:19 +0700

    split array

--- text_in_eeprom/text_in_eeprom.ino
+++ text_in_eeprom/text_in_eeprom.ino
@@ -1,21 +1,37 @@
 #include <EEPROM.h>
-float value;
-float x = 123.45;
-char txt[30] ;
-char data[] = "text in eeprom";
+char tmpData[300];
+String stringOne = "1,2,3,4,5,6,#####0:47520:60#0:47520:60";
+String arrServResp[6];
 void setup() {
   EEPROM.begin(512);  // setup
   Serial.begin(115200);
-  // write data (put use update function)
-  EEPROM.put(0, x);
-  EEPROM.put(sizeof(x), data);
+
+  // Write data to eeprom
+  stringOne.toCharArray(tmpData, 300);
+  EEPROM.put(0, tmpData);
   EEPROM.commit();
 
-  // read data
-  EEPROM.get(0, value);
-  EEPROM.get(sizeof(x), txt);
+  // Read data from eeprom
+  EEPROM.get(0, tmpData);
+  String serverResponse = String(tmpData);
+
+  //=== split server response to arrServResp array
+  //String serverResponse = "weekOfDay,minInDay,relayActiveInWeek";
+  char sz[300];
+  serverResponse.toCharArray(sz, sizeof(sz));
+
+  // split string to array
+  char *str, *p = sz;
+  int i = 0;
+  while (str = strtok (p, ",")){
+    arrServResp[i] = str;
+    p = NULL;
+    ++i;
+  }
+
+  
 }
 void loop() {
-  Serial.println(value);
-  Serial.println(txt);
+  Serial.println(arrServResp[5]);
+  Serial.println(arrServResp[4]);
 }
