05482cf4613fda878bb22c2cbcc735c43499ae9f
weaq <30413309+weaq@users.noreply.github.com>
2022-02-25 22:36:57 +0700

    read write string to eeprom

--- text_in_eeprom/text_in_eeprom.ino
+++ text_in_eeprom/text_in_eeprom.ino
@@ -1,37 +1,44 @@
 #include <EEPROM.h>
-char tmpData[300];
-String stringOne = "1,2,3,4,5,6,#####0:47520:60#0:47520:60";
-String arrServResp[6];
+
+String data = "{\"freedom\":{\"0\":\"1\",\"1\":\"2\",\"2\":\"2\",\"3\":\"1\"},\"wDay\":{\"0\":{\"0\":\"1\",\"1\":\"1\",\"2\":\"1\",\"3\":\"1\",\"4\":\"1\",\"5\":\"1\",\"6\":\"1\"},\"1\":{\"0\":\"1\",\"1\":\"1\",\"2\":\"1\",\"3\":\"1\",\"4\":\"1\",\"5\":\"1\",\"6\":\"1\"},\"2\":{\"0\":\"1\",\"1\":\"1\",\"2\":\"1\",\"3\":\"1\",\"4\":\"1\",\"5\":\"1\",\"6\":\"1\"},\"3\":{\"0\":\"1\",\"1\":\"1\",\"2\":\"1\",\"3\":\"1\",\"4\":\"1\",\"5\":\"1\",\"6\":\"1\"}},\"sTime\":{\"0\":{\"0\":82800,\"1\":82800,\"2\":82800,\"3\":82800},\"1\":{\"0\":82800,\"1\":82800,\"2\":82800,\"3\":82800},\"2\":{\"0\":82800,\"1\":82800,\"2\":82800,\"3\":82800},\"3\":{\"0\":82800,\"1\":82800,\"2\":82800,\"3\":82800}},\"aTime\":{\"0\":{\"0\":\"3600\",\"1\":\"3600\",\"2\":\"3600\",\"3\":\"3600\"},\"1\":{\"0\":\"3600\",\"1\":\"3600\",\"2\":\"3600\",\"3\":\"3600\"},\"2\":{\"0\":\"3600\",\"1\":\"3600\",\"2\":\"3600\",\"3\":\"3600\"},\"3\":{\"0\":\"3600\",\"1\":\"3600\",\"2\":\"3600\",\"3\":\"3600\"}},\"unitTime\":4}";
+
 void setup() {
-  EEPROM.begin(512);  // setup
-  Serial.begin(115200);
-
-  // Write data to eeprom
-  stringOne.toCharArray(tmpData, 300);
-  EEPROM.put(0, tmpData);
-  EEPROM.commit();
-
-  // Read data from eeprom
-  EEPROM.get(0, tmpData);
-  String serverResponse = String(tmpData);
-
-  //=== split server response to arrServResp array
-  //String serverResponse = "weekOfDay,minInDay,relayActiveInWeek";
-  char sz[300];
-  serverResponse.toCharArray(sz, sizeof(sz));
-
-  // split string to array
-  char *str, *p = sz;
-  int i = 0;
-  while (str = strtok (p, ",")){
-    arrServResp[i] = str;
-    p = NULL;
-    ++i;
-  }
-
+  Serial.begin(115220);
+  EEPROM.begin(1024);
   
+  Serial.print("Write Data:");
+  Serial.println(data);
+
+  eeprom_write(10, data);  // Write at address 10
+  delay(10);
 }
+
 void loop() {
-  Serial.println(arrServResp[5]);
-  Serial.println(arrServResp[4]);
+  Serial.println(data.length());
+  String recivedData = eeprom_read(10); // Read at address 10
+  Serial.print("Read Data:");
+  Serial.println(recivedData);
+  delay(1000);
+}
+
+void eeprom_write(char add,String data) {
+  int _size = data.length();
+  for(int i=0;i<_size;i++) {
+    EEPROM.write(add+i,data[i]);
+  }
+  EEPROM.write(add+_size,'\0');   // Add termination null character for String Data
+  EEPROM.commit();
+}
+
+String eeprom_read(char add) {
+  char data[1024]; // Max 1024 Bytes
+  int len=0;
+  unsigned char k = EEPROM.read(add);
+  while(k != '\0' && len<1024) {   // Read until null character
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
 }
